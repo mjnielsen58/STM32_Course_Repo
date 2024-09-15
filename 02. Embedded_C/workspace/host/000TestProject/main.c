8dfafd5913fca65bfdb092e3974b636f30948286
Michael Juel Nielsen <mjnielsen@live.dk>
2024-09-15 18:30:52 +0200

    After importing existing projects
    01. Mastering_STM32:
    Section-01\
    Section-02\EX01_InputOutput-Driver\
    01. Mastering_STM32:
    instructor\host\
    instructor\target\
    workspace\host\
    workspace\target\

---
+++ 02. Embedded_C/workspace/host/000TestProject/main.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/*
+ * main.c
+ *
+ *  Created on: Sep 14, 2024
+ *      Author: mjnie
+ */
+
+int	main(void){
+	printf("Hello world\n");
+
+	printf("Press 'Enter' to exit this application\n");
+
+	getchar();
+
+	return 0;
+
+}
